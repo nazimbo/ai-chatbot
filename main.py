f760e9f8f51194b261d2ed0eb119bc6287a08844
nazimbo <113207575+nazimbo@users.noreply.github.com>
2024-01-18 22:01:40 +0100

    Update chatbot API model

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ with st.sidebar:
 st.title("Charlie AI")
 st.caption("Un chatbot utilisant l'API OpenAI.")
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state["messages"] = [{"role": "assistant", "content": "Comment puis-je vous aider ?"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
@@ -21,7 +21,7 @@ if prompt := st.chat_input():
     client = OpenAI(api_key=openai_api_key)
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    response = client.chat.completions.create(model="gpt-4", messages=st.session_state.messages)
     msg = response.choices[0].message.content
     st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
